c4b2b130c1638b1e94c0774510914ac1eeb946d3
Martin Bless <martin@mbless.de>
2016-08-12 15:34:11 +0200

    Add install commands for Contrib tools

--- Contrib/install.source-me.sh
+++ Contrib/install.source-me.sh
@@ -4,6 +4,6 @@
 #   (venv)$  cd path/to/tct/Contrib
 #   (venv)$  source install.source-me.sh
 
-cp -p check_include_files.py $($PATH | cut -d : -f 1)
-cp -p jq                     $($PATH | cut -d : -f 1)
+cp -p check_include_files.py $(echo $PATH | cut -d : -f 1)
+cp -p jq                     $(echo $PATH | cut -d : -f 1)
 
