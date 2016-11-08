e6e0efbc8a6f362331b693a4054fa09b551661a8
Martin Bless <martin@mbless.de>
2016-11-08 17:58:21 +0100

    [BUGFIX] Bump version 0.1.1 -> 0.1.2 (important!)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='TCT Toolchain Tool',
-    version='0.1.1',
+    version='0.1.2',
     py_modules=['tct'],
     packages= ['tctlib'],
     include_package_data=True,
