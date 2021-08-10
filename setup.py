c9387d08839183c4640637b9d8be008fa5747d50
Martin Bless <martin.bless@mbless.de>
2021-08-10 14:06:39 +0200

    Just reformat using 'black'

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from __future__ import absolute_import
 from setuptools import setup
 
 setup(
-    name='TCT Toolchain Tool',
-    version='1.1.0',
-    py_modules=['tct'],
-    packages= ['tctlib'],
+    name="TCT Toolchain Tool",
+    version="1.1.0",
+    py_modules=["tct"],
+    packages=["tctlib"],
     include_package_data=True,
     install_requires=[
-        'click',
-        'pyyaml',
+        "click",
+        "pyyaml",
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
         tct=tct:cli
-    ''',
+    """,
 )
--- tctlib/__init__.py
+++ tctlib/__init__.py
@@ -7,18 +7,19 @@ import os
 import time
 
 __all__ = [
-    'data2json',
-    'deepget',
-    'finder',
-    'logstamp',
-    'logstamp_finegrained',
-    'make_snapshot_of_milestones',
-    'msecs',
-    'readjson',
-    'save_the_result',
-    'versiontuple',
-    'writejson',
+    "data2json",
+    "deepget",
+    "finder",
+    "logstamp",
+    "logstamp_finegrained",
+    "make_snapshot_of_milestones",
+    "msecs",
+    "readjson",
+    "save_the_result",
+    "versiontuple",
+    "writejson",
 ]
+
 
 def finder(dirpath):
     # Usage:
@@ -27,6 +28,7 @@ def finder(dirpath):
     #
     dirpathlen = len(dirpath)
     initialdepth = dirpath.count(os.path.sep)
+
     def finder1():
         for root, dirs, files in os.walk(dirpath):
             depth = root.count(os.path.sep) - initialdepth
@@ -34,13 +36,25 @@ def finder(dirpath):
             files.sort()
             for file in files:
                 fpath = os.path.join(root, file)
-                if os.path.isfile(fpath) and os.access(fpath, os.X_OK) and file.startswith('run_'):
-                    yield (depth, dirpath, root[dirpathlen:].strip(os.path.sep), file, fpath)
+                if (
+                    os.path.isfile(fpath)
+                    and os.access(fpath, os.X_OK)
+                    and file.startswith("run_")
+                ):
+                    yield (
+                        depth,
+                        dirpath,
+                        root[dirpathlen:].strip(os.path.sep),
+                        file,
+                        fpath,
+                    )
+
     return finder1
 
+
 def deepget(dictionary, *keys, **kwargs):
-    if 'default' in kwargs:
-        default = kwargs['default']
+    if "default" in kwargs:
+        default = kwargs["default"]
     else:
         default = {}
     result = dictionary
@@ -58,8 +72,8 @@ def make_snapshot_of_milestones(milestonesfile_abspath, paramsfile_abspath):
 
     paramsfile_folder, paramsfile_name = os.path.split(paramsfile_abspath)
     # do a rough check
-    if paramsfile_name.startswith('params_'):
-        dest = os.path.join(paramsfile_folder, 'milestones_' + paramsfile_name[7:])
+    if paramsfile_name.startswith("params_"):
+        dest = os.path.join(paramsfile_folder, "milestones_" + paramsfile_name[7:])
         shutil.copy(milestonesfile_abspath, dest)
 
 
@@ -72,45 +86,53 @@ def msecs(unixtime=None):
     # return '1469006952733'
     return str(int(unixtime * 1000))
 
-def logstamp(unixtime=None, fmt='%Y-%m-%d %H:%M'):
+
+def logstamp(unixtime=None, fmt="%Y-%m-%d %H:%M"):
     "Return a timestamp suitable for logging like '2016-07-26 21:05'"
 
     if unixtime is None:
         unixtime = time.time()
     return datetime.datetime.fromtimestamp(unixtime).strftime(fmt)
 
-def logstamp_finegrained(unixtime=None, fmt='%Y-%m-%d_%H-%M-%S_%f'):
+
+def logstamp_finegrained(unixtime=None, fmt="%Y-%m-%d_%H-%M-%S_%f"):
     "Return fine grained timestamp like `2016-07-26_21-05-59_888999`."
 
     return logstamp(unixtime, fmt=fmt)
 
+
 def readjson(fpath):
     result = None
-    with codecs.open(fpath, 'r', 'utf-8', errors='replace') as f1:
+    with codecs.open(fpath, "r", "utf-8", errors="replace") as f1:
         result = json.load(f1)
     return result
 
+
 def writejson(data, fpath):
-    with codecs.open(fpath, 'w', 'utf-8', errors='replace') as f2:
-        json.dump(data, f2, sort_keys=True, indent=2, separators=(',', ': '))
+    with codecs.open(fpath, "w", "utf-8", errors="replace") as f2:
+        json.dump(data, f2, sort_keys=True, indent=2, separators=(",", ": "))
     return
+
 
 def data2json(data):
     io = StringIO()
-    json.dump(data, io, sort_keys=True, indent=2, separators=(',', ': '))
+    json.dump(data, io, sort_keys=True, indent=2, separators=(",", ": "))
     return io.getvalue()
 
-def versiontuple(v, n=12):
-   filled = []
-   for point in v.split("."):
-      filled.append(point.zfill(n))
-   return tuple(filled)
 
-def save_the_result(result, resultfile, params, facts, milestones, exitcode,
-                    CONTINUE, reason):
-    tool_exitcodes_2 = milestones.get('tool_exitcodes_2', {})
-    k = '%s/%s' % (params['toolrelpath'], params['toolname'])
-    tool_exitcodes_2[k] = '%s,%s,%s' % (exitcode, CONTINUE, reason)
-    result['MILESTONES'].append({'tool_exitcodes_2': tool_exitcodes_2})
+def versiontuple(v, n=12):
+    filled = []
+    for point in v.split("."):
+        filled.append(point.zfill(n))
+    return tuple(filled)
+
+
+def save_the_result(
+    result, resultfile, params, facts, milestones, exitcode, CONTINUE, reason
+):
+    tool_exitcodes_2 = milestones.get("tool_exitcodes_2", {})
+    k = "%s/%s" % (params["toolrelpath"], params["toolname"])
+    tool_exitcodes_2[k] = "%s,%s,%s" % (exitcode, CONTINUE, reason)
+    result["MILESTONES"].append({"tool_exitcodes_2": tool_exitcodes_2})
     writejson(result, resultfile)
     return True
